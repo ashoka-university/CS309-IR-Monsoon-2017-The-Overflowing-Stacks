0af191e1ecde3f2d8de1cf9d0fdf0332ee599f58
vijaylingam <vijay.lingam@ashoka.edu.in>
2017-10-29 16:20:19 +0530

    Added 3 new models
    Model 1 is a plain LSTM, which extracts features from both Sentence A
    and Sentence B and concatenates them and this is followed by 3 fully
    connected dense layers.
    Model 1 stats: 78.53% training accuracy and 56.80% test accuracy
    Impressions: Overfitting, there is scope to reduce avoidable bias and
    variance
    
    Model 2 uses a shared LSTM (both sentence A and sentence B share the
    same weights). We have increased the embedding output dimensionality to
    64. The number memory units in LSTM layer is also now increased to 64.
    Stats: 80.97% training accuracy, 55.60% test accuracy
    Impressions: Overfitting training data, no regularisation techniques
    deployed
    
    Model 3 uses a bi-directional LSTM and uses concatenate mechanism.
    Stats: 92.55% training accuracy, 50% test accuracy
    Impressions: Overfitting on training data, biLSTM learns much faster,
    no regularisation used
    
    All the models were trained on 4000 sentences and tested on 500
    sentences (All the data is from same distribution). Each model was
    trained for 25 epochs.

--- model.py
+++ model.py
@@ -3,8 +3,11 @@ from sklearn.preprocessing import LabelEncoder
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import LSTM, Bidirectional
 from keras.layers.embeddings import Embedding
+from keras.layers import Input
+from keras.models import Model
+import keras
 from keras.preprocessing import sequence
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -65,15 +68,14 @@ for i in range(len(sentenceA)):
 	for word in sentenceB[i].split():
 		embeddedVecB.append(char_indices.get(word))
 
-	embeddedVecA +=  [0]*(30 - len(embeddedVecA))
-	embeddedVecB +=  [0]*(30 - len(embeddedVecB))
+	embeddedVecA +=  [0]*(40 - len(embeddedVecA))
+	embeddedVecB +=  [0]*(40 - len(embeddedVecB))
 
 	vectorA.append(embeddedVecA)
 	vectorB.append(embeddedVecB)
 
-for line in vectorA:
-	print(line)
-
+# for line in vectorA:
+# 	print(line)
 
 encoder = LabelEncoder()
 encoder.fit(classLabel)
@@ -91,15 +93,106 @@ NEUTRAL:		0 0 1
 #---------------------------------------------------------#
 
 vectorA = np.array(vectorA)
+vectorB = np.array(vectorB)
 onehot = np.array(onehot)
 
-X_train, X_test, y_train, y_test = train_test_split(vectorA, onehot, test_size=0.30, random_state=7)
+print(len(vectorA))
+print(len(vectorB))
+#X_train, X_test, y_train, y_test = train_test_split([vectorA, vectorB], onehot, test_size=0.30, random_state=7)
 
-embedding_vecor_length = 32
-model = Sequential()
-model.add(Embedding(2193, embedding_vecor_length, input_length = 30))
-model.add(LSTM(100))
-model.add(Dense(3, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+# print(X_train[0])
+# print(y_train[0])
+
+# for i in vectorB:
+# 	if len(i) != 30:
+# 		print(i)
+
+
+print(vectorA.shape)
+print(vectorB.shape)
+
+vecAtrain = vectorA[:4000]
+vecBtrain = vectorB[:4000]
+y_train = onehot[:4000]
+
+vecAtest = vectorA[4000:]
+vecBtest = vectorB[4000:]
+y_test = onehot[4000:]
+
+
+#*********** Separate LSTM's for each sentence ******************
+# loss: 0.4934 - acc: 0.7853 - val_loss: 0.9514 - val_acc: 0.5680
+#****************************************************************
+
+# senA = Input(shape=(40,), dtype='int32', name='senA')
+# x = Embedding(output_dim=32, input_dim=2193, input_length=30)(senA)
+# senB = Input(shape=(40,), dtype='int32', name='senB')
+# y = Embedding(output_dim=32, input_dim=2193, input_length=30)(senB)
+# lstm_out1 = LSTM(48)(x)
+# # lstm_out1 = LSTM(32)(lstm_out1)
+# lstm_out2 = LSTM(48)(y)
+# # lstm_out2 = LSTM(32)(lstm_out2)
+# x = keras.layers.concatenate([lstm_out1, lstm_out2])
+# x = Dense(64, activation='relu')(x)
+# x = Dense(32, activation='relu')(x)
+# x = Dense(16, activation='relu')(x)
+# main_output = Dense(3, activation='softmax', name='main_output')(x)
+
+# model = Model(inputs=[senA, senB], outputs=[main_output])
+# model.compile(optimizer='rmsprop', loss='categorical_crossentropy',  metrics=['accuracy'])
+# model.fit([vecAtrain, vecBtrain], [y_train], validation_data=([vecAtest, vecBtest], y_test),verbose=1, epochs=25, batch_size=32)
+
+#********************* Shared LSTM Model ************************
+# loss: 0.4423 - acc: 0.8097 - val_loss: 1.0831 - val_acc: 0.5560
+#****************************************************************
+
+# senA = Input(shape=(40,), dtype='int32', name='senA')
+# x = Embedding(output_dim=64, input_dim=2193, input_length=30)(senA)
+# senB = Input(shape=(40,), dtype='int32', name='senB')
+# y = Embedding(output_dim=64, input_dim=2193, input_length=30)(senB)
+
+# shared_lstm = LSTM(64)
+
+# encoded_a = shared_lstm(x)
+# encoded_b = shared_lstm(y)
+
+# merged_vector = keras.layers.concatenate([encoded_a, encoded_b], axis=-1)
+
+# main_output = Dense(3, activation='softmax', name='main_output')(merged_vector)
+
+# model = Model(inputs=[senA, senB], outputs=[main_output])
+# model.compile(optimizer='rmsprop', loss='categorical_crossentropy',  metrics=['accuracy'])
+# model.fit([vecAtrain, vecBtrain], [y_train], validation_data=([vecAtest, vecBtest], y_test),verbose=1, epochs=25, batch_size=32)
+
+#******************** Bi-directional LSTM ***********************
+# loss: 0.1873 - acc: 0.9255 - val_loss: 1.9496 - val_acc: 0.5000
+#****************************************************************
+
+senA = Input(shape=(40,), dtype='int32', name='senA')
+x = Embedding(output_dim=64, input_dim=2193, input_length=30)(senA)
+senB = Input(shape=(40,), dtype='int32', name='senB')
+y = Embedding(output_dim=64, input_dim=2193, input_length=30)(senB)
+
+shared_lstm = Bidirectional(LSTM(64))
+
+encoded_a = shared_lstm(x)
+encoded_b = shared_lstm(y)
+
+merged_vector = keras.layers.concatenate([encoded_a, encoded_b], axis=-1)
+
+main_output = Dense(3, activation='softmax', name='main_output')(merged_vector)
+
+model = Model(inputs=[senA, senB], outputs=[main_output])
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',  metrics=['accuracy'])
+model.fit([vecAtrain, vecBtrain], [y_train], validation_data=([vecAtest, vecBtest], y_test),verbose=1, epochs=25, batch_size=32)
+
+
+
+# embedding_vecor_length = 32
+# model = Sequential()
+# model.add(Embedding(2193, embedding_vecor_length, input_length = 30))
+# model.add(LSTM(100))
+# model.add(Dense(3, activation='softmax'))
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# print(model.summary())
+# model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
